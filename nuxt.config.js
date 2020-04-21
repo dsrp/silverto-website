9de60d1de58bf1a8d422411c15107c09e640e7f5
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-04-21 21:03:46 +0100

    Include video poster file.

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,11 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend (config, { isClient, loaders: { vue } }) {
+      // Extend only webpack config for client-bundle
+      if (isClient) {
+        vue.transformAssetUrls.video = ['src', 'poster']
+      }
     }
   }
 }
