ffa72c1acf80fcc21156af70e03bcec623038272
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-04-22 10:02:05 +0100

    All pages working!

---
+++ pages/about.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="about">
+    <h1>Sobre nos</h1>
+    <h2>Cooperativa Ecoaldeia Silverto</h2>
+    <h3>Combina co-habitação rural com agroflorestação regenerativa.</h3>
+    <p>Conviver em e com a natureza, desenvolver a economia rural, revigorar a comunidade. Pretendemos fazer um exemplo de regeneração rural do região, mostrar novas práticas de uso da terra, revalorizar as qualidades únicas do campo.</p>
+    <h3>Evolução</h3>
+    <p>Atualmente estamos comprando um campo de 1,5 ha de floresta com direitos de construção. Em 2019 esperamos renovar a casa existente, que terá uma função comum (co-working, cozinha, guestroom). Também iniciaremos a gestão ativa da floresta no campo, para torná-la comercialmente viável. Até 2022, queremos abrigar uma comunidade de até cinco domicílios, fazendo auto-construção de casas confortáveis e económicas, com baixo impacto ecológico. Estamos visando um máximo total de 12 pessoas.</p>
+    <h3>Onde estamos</h3>
+    <iframe
+      width="425"
+      height="350"
+      frameborder="0"
+      scrolling="no"
+      marginheight="0"
+      marginwidth="0"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=-9.354858398437502%2C41.47360232634395%2C-7.871704101562501%2C42.32301644617188&amp;layer=mapnik&amp;marker=41.899721690058364%2C-8.61328125"
+      style="border: 1px solid black"
+    /><br><small><a href="https://www.openstreetmap.org/?mlat=41.8997&amp;mlon=-8.6133#map=10/41.8997/-8.6133">Visualizar Mapa em Tamanho Maior</a></small>
+    <h3>Morada</h3>
+    <address>
+      Caminho de Moreira nº240<br>
+      Silverto, Rubiães<br>
+      4940-692 Paredes de Coura<br>
+      Portugal
+    </address>
+    <h3>Orgãos Sociais</h3>
+    <ul class="inline">
+      <li>
+        <h4>Administrador</h4>
+        Mathijs de Bruin
+      </li>
+      <li>
+        <h4>Assembleia Geral</h4>
+        Nuno Reis Pereira
+      </li>
+      <li>
+        <h4>Fiscalização</h4>
+        Diego Reimondez
+      </li>
+    </ul>
+
+    <h3>Parceiros</h3>
+    <ul class="inline">
+      <li><a href="https://solnascente.eu/"><img alt="Sol Nascente" src="@/assets/solnascente-logo-light-small.svg"></a></li>
+      <li><a href="https://movingcause.org/"><img alt="Moving Cause" src="@/assets/movingcause-logo.png"></a></li>
+      <li><a href="https://reflorestar-portugal.com"><img alt="Reflorestar Portugal" src="@/assets/reflorestar-logo.png"></a></li>
+      <li><a href="https://dsrp.eu"><img alt="Decentralised Society Research Project" src="@/assets/dsrp-logo.png"></a></li>
+    </ul>
+
+    <h3>Fale conosco</h3>
+    <p>Estamos felizes de te-encontrar! Mande-nos um email para iniciar a conversa!</p>
+    <p><a href="mailto:info@silverto.pt">info@silverto.pt</a></p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+
+  > * {
+    max-width: 80vw;
+
+    @media only screen and (min-width: 500px) {
+      max-width: 40em;
+    }
+  }
+
+  padding: 1em;
+
+  address, p {
+    text-align: left;
+  }
+
+  ul.inline {
+    @media only screen and (min-width: 500px) {
+      max-width: 100em;
+    }
+
+    list-style-type: none;
+
+    > li {
+      display: inline;
+      padding: 10pt;
+
+      > h4 {
+        display: inline;
+      }
+
+      img {
+        padding-bottom: 6pt;
+        height: 90px;
+        max-width: 60vw;
+      }
+
+    }
+  }
+}
+</style>
---
+++ pages/subscribe.vue
@@ -0,0 +1,82 @@
+<template>
+  <!-- Begin Mailchimp Signup Form -->
+  <div id="mc_embed_signup" class="newsletter">
+    <form
+      id="mc-embedded-subscribe-form"
+      action="https://Solnascente.us20.list-manage.com/subscribe/post?u=4951e3c30865c87767a6a4b0d&amp;id=807bea2a36"
+      method="post"
+      name="mc-embedded-subscribe-form"
+      class="validate"
+      target="_blank"
+      novalidate
+    >
+      <div id="mc_embed_signup_scroll">
+        <h2>Inscrever a nossa newsletter</h2>
+        <p>Receba notícias sobre Silverto e Sol Nascente no maximo cadas duas semanas.</p>
+        <div class="indicates-required">
+          <span class="asterisk">*</span> indicates required
+        </div>
+        <div class="mc-field-group">
+          <label for="mce-EMAIL">Email<span class="asterisk">*</span>
+          </label>
+          <input id="mce-EMAIL" type="email" value="" name="EMAIL" class="required email">
+        </div>
+        <div class="mc-field-group">
+          <label for="mce-FNAME">Nome</label>
+          <input id="mce-FNAME" type="text" value="" name="FNAME" class="">
+        </div>
+        <div class="mc-field-group">
+          <label for="mce-LNAME">Apelido</label>
+          <input id="mce-LNAME" type="text" value="" name="LNAME" class="">
+        </div>
+        <p>
+          <a href="https://us20.campaign-archive.com/home/?u=4951e3c30865c87767a6a4b0d&id=807bea2a36" title="View previous campaigns">Ver notícias anteriores.</a>
+        </p>
+        <div id="mce-responses" class="clear">
+          <div id="mce-error-response" class="response" style="display:none" />
+          <div id="mce-success-response" class="response" style="display:none" />
+        </div>
+
+        <!-- Silverto group -->
+        <input id="group_8" type="hidden" name="group[4065][8]" value="1">
+
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+          <input type="text" name="b_4951e3c30865c87767a6a4b0d_807bea2a36" tabindex="-1" value="">
+        </div>
+        <div class="clear">
+          <input id="mc-embedded-subscribe" type="submit" value="Subscribe" name="subscribe" class="button">
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <!--End mc_embed_signup-->
+</template>
+
+<script>
+</script>
+
+<style  lang="scss" scoped>
+@import '~assets/mailchimp.css';
+
+#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+
+.newsletter {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+
+  > * {
+    min-width: 40vw;
+    max-width: 80vw;
+
+    @media only screen and (min-width: 500px) {
+      max-width: 40em;
+    }
+  }
+
+  padding: 1em;
+}
+
+</style>
