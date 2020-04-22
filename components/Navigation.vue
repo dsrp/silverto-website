7904df6db65d6cd7bfe9a2b2d0dcafc81eb111c1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-04-22 16:20:47 +0100

    Split out Navigation component.
    Also remove Logo component.

---
+++ components/Navigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="navigation">
+    <nav class="menu">
+      <nuxt-link :to="localePath('/')">
+        {{ $t('home') }}
+      </nuxt-link> |
+      <nuxt-link :to="localePath('/about')">
+        {{ $t('about') }}
+      </nuxt-link> |
+      <nuxt-link :to="localePath('/subscribe')">
+        {{ $t('subscribe') }}
+      </nuxt-link>
+    </nav>
+
+    <nav class="lang">
+      <nuxt-link
+        v-if="$i18n.locale !== 'en'"
+        :to="switchLocalePath('en')"
+      >
+        English
+      </nuxt-link>
+      <nuxt-link
+        v-if="$i18n.locale !== 'pt'"
+        :to="switchLocalePath('pt')"
+      >
+        Português
+      </nuxt-link>
+    </nav>
+  </div>
+</template>
+
+<i18n lang="yaml">
+en:
+  home: Home
+  about: About
+  subscribe: Subscribe
+
+pt:
+  about: Sobre nos
+  subscribe: Inscrever
+</i18n>
+
+<style lang="scss" scoped>
+@import '~assets/scss/colors.scss';
+
+.navigation {
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-between;
+
+  padding: 7pt;
+  background-color: transparentize($dark-green, 0.85);
+  a {
+    font-weight: bold;
+    color: white;
+    &.nuxt-link-exact-active {
+      color: $orange;
+      text-decoration: none;
+    }
+    &:hover {
+      text-decoration: none;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,7 @@
 <template>
   <div id="app">
     <header class="header">
-      <nav class="menu">
-        <nuxt-link :to="localePath('/')">
-          {{ $t('home') }}
-        </nuxt-link> |
-        <nuxt-link :to="localePath('/about')">
-          {{ $t('about') }}
-        </nuxt-link> |
-        <nuxt-link :to="localePath('/subscribe')">
-          {{ $t('subscribe') }}
-        </nuxt-link>
-      </nav>
-
-      <nav class="lang">
-        <nuxt-link
-          v-if="$i18n.locale !== 'en'"
-          :to="switchLocalePath('en')"
-        >
-          English
-        </nuxt-link>
-        <nuxt-link
-          v-if="$i18n.locale !== 'pt'"
-          :to="switchLocalePath('pt')"
-        >
-          Português
-        </nuxt-link>
-      </nav>
+      <Navigation />
     </header>
 
     <main class="main">
@@ -35,25 +10,22 @@
   </div>
 </template>
 
-<i18n lang="yaml">
-en:
-  home: Home
-  about: About
-  subscribe: Subscribe
+<script>
+import Navigation from '@/components/Navigation.vue'
 
-pt:
-  about: Sobre nos
-  subscribe: Inscrever
-</i18n>
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
 
 <style lang="scss">
 @import 'normalize-scss';
 @include normalize();
 
-$dark-green: #19341A;
-$medium-green: #50A54C;
-$light-green: #B7D433;
-$orange: #EFB81D;
+@import '~assets/scss/colors.scss';
+
 html, body {
   height: 100%;
   margin: 0;
@@ -67,35 +39,6 @@ html, body {
   display: flex;
   min-height: 100vh;
   flex-flow: column;
-}
-nav {
-  .lang {
-    align-items: right;
-  }
-}
-
-header.header {
-  display: flex;
-  flex-flow: column;
-  align-items: center;
-
-  .lang {
-    align-self: right;
-  }
-
-  padding: 7pt;
-  background-color: transparentize($dark-green, 0.85);
-  a {
-    font-weight: bold;
-    color: white;
-    &.nuxt-link-exact-active {
-      color: $orange;
-      text-decoration: none;
-    }
-    &:hover {
-      text-decoration: none;
-    }
-  }
 }
 .main {
   flex: 1;
